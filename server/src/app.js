e142d0488b410b838f4a761bf9d5d6d9040d8561
Todd Powell <toddwpowell@gmail.com>
2017-12-16 23:01:27 -0700

    Set up basic server

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+console.log("hello");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('combined')); //logging
+app.use(bodyParser.json()); //parse json that comes in
+app.use(cors());    // allow any client to access this - needed if you want a server to be hosted on a different domain and to let any client from around the world to hit your server.
+
+// Create a simple endpoint
+app.get('/status', (req, res) => {
+    res.send({
+        message: "Hello, world"
+    })
+})
+
+app.listen(process.env.PORT || 8081);
