c5afe23b8753fcfbadeb7db5f3c1e3f1f5dec073
Todd Powell <toddwpowell@gmail.com>
2017-12-24 11:07:20 -0700

    Added jwtToken to register endpoint

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -14,7 +14,11 @@ module.exports = {
     async register (req, res) {
         try{
             const user = await User.create(req.body)
-            res.send(user.toJSON())
+            const userJson = user.toJSON()
+            res.send({
+                user: user.toJSON(),
+                token: jwtSignUser(userJson)
+            })
         } catch (err) {
             console.log("ERROR - " + err)
             res.status(400).send({
