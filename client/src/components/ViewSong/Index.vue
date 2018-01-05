1fdf7623c220d65556ee732f1d34e2e43aaa741b
Todd Powell <toddwpowell@gmail.com>
2018-01-04 19:48:35 -0700

    Added Lyrics and Tab views

--- client/src/components/ViewSong/Index.vue
+++ client/src/components/ViewSong/Index.vue
@@ -10,6 +10,16 @@
             </v-flex>
         </v-layout>
 
+        <v-layout class="mt-2">
+            <v-flex xs6 class="ml-2">
+                <tab :song="song"/>
+            </v-flex>
+
+            <v-flex xs6 class="ml-2">
+                <lyrics :song="song"/>
+            </v-flex>
+        </v-layout>
+
         <!-- <v-layout>
             <v-flex xs6>
                 <panel title="YouTube Video">
@@ -33,6 +43,8 @@ import Panel from "@/components/Panel"
 import SongMetadata from './SongMetadata'
 import SongsService from '@/services/SongsService'
 import YouTube from './YouTube'
+import Lyrics from './Lyrics'
+import Tab from './Tab'
 
 export default {
     data () {
@@ -50,7 +62,9 @@ export default {
     components: {
         Panel,
         SongMetadata,
-        YouTube
+        YouTube,
+        Lyrics,
+        Tab
     }
 }
 </script>
---
+++ client/src/components/ViewSong/Tab.vue
@@ -0,0 +1,33 @@
+<template>
+  <panel title="Tab">
+    <textarea
+        readonly
+        v-model="song.tab">
+        </textarea>
+  </panel>
+</template>
+
+<script>
+import Panel from "@/components/Panel"
+export default {
+  props: [
+      "song"
+  ],
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+textarea {
+    width: 100%;
+    font-family: monospace;
+    border-style: none;
+    border-color: transparent;
+    /* height: 600px; */
+    height: 250px;
+    overflow: auto;
+    padding: 20px;
+}
+</style>
